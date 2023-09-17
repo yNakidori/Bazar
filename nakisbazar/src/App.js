dd98cfbee93ce80c21d37cf9cf1a0214478a03ec
naki <leandroreisubi@gmail.com>
2023-09-17 14:15:29 -0300

    commit 1

---
+++ nakisbazar/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import{Route, Routes, BrowserRouter} from 'react-router-dom'
+import Home from './PAGES/HomePage/Home'
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="home" element={<Home />} />
+      <Route path="*" element={<div>
+        <h1>404 NOT FOUND</h1>
+      </div>} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
